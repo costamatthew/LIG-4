8afd99088457c3bc4cd33273be77c76b980e622e
Leandro Arruda <nascimentoarruda123@gmail.com>
2021-01-05 15:14:54 -0300

    adicionado mapa funcional

--- functions.js
+++ functions.js
@@ -1,66 +1,62 @@
+const body = document.querySelector('body')
+const painel = document.getElementById('painel')
 const colunas = document.querySelectorAll(".tabuleiro_coluna");
+
+const vertical = ['','','','','','']
+const horizontal = [[''],[''],[''],[''],[''],[''],['']]
+const diagonal = [[''],[''],[''],[''],[''],[''],[''],[''],[''],[''],[''],['']]
+const diagonal2 = [[''],[''],[''],[''],[''],[''],[''],[''],[''],[''],[''],['']]
 
 let keep;
 let turn = 1;
 
 colunas.forEach(function (colfor) {
+
     colfor.addEventListener("click", function(e) {
 
-        let limit = e.currentTarget.children.length
+        const collumn = e.currentTarget
+        const id = collumn.id
+        const limit = e.currentTarget.children.length
         
+        function Gerardisco(string) {
+            let d1 = document.createElement("div");
+            d1.setAttribute("id", string);
+            d1.style.backgroundColor = string
+            d1.style.width = "80%";
+            d1.style.height = "10%";
+            d1.style.borderRadius = "40%";
+            d1.style.padding = "10px";
+            d1.style.margin = "10px";
+            vertical[id] += string[0]
+            horizontal[parseInt(limit)][id] = string[0]
+            diagonal[parseInt(id)+parseInt(limit)][id] = string[0]
+            diagonal2[6-parseInt(id)+parseInt(limit)][id] = string[0]
+            return d1;
+        }
+
         if (limit < 6) {
             if (turn === 1) {
-                let event = e.target.id;
-                keep = document.getElementById(event);
-                keep.appendChild(disco_1());
-                //console.log(keep)
-            }
-    
-            if (turn === 2) {
-                let event = e.target.id;
-                keep = document.getElementById(event);
-                keep.appendChild(disco_2());
-                //console.log(keep)
+                collumn.appendChild(Gerardisco('yellow'));
             }
 
-            playerTurn(turn);
-            console.log(turn);
+            if (turn === 2) {
+                collumn.appendChild(Gerardisco('blue'));
+            }
+
+            playerTurn(turn)
         }
     })
 })
 
 function playerTurn(e) {
     if (e === 1) {
-
         turn = 2;
     } else {
         turn = 1;
     } 
 }
 
-function disco_1() {
-    let d1 = document.createElement("div");
-    d1.setAttribute("id", "disco1");
-    d1.style.backgroundColor = "#000";
-    d1.style.width = "80%";
-    d1.style.height = "10%";
-    d1.style.borderRadius = "40%";
-    d1.style.padding = "10px";
-    d1.style.margin = "10px";
-    return d1;
-}
 
-function disco_2() {
-    let d2 = document.createElement("div");
-    d2.setAttribute("id", "disco2");
-    d2.style.backgroundColor = "#F30303";
-    d2.style.width = "80%";
-    d2.style.height = "10%";
-    d2.style.borderRadius = "40%";
-    d2.style.padding = "10px";
-    d2.style.margin = "10px";
-    return d2;
-}
 
 
 
